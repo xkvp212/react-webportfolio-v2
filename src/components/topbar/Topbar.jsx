08ffd6f40fb172169d93ecd2345b96252d72632d
Kevin <GIT SERVICE EMAIL>
2021-06-01 13:05:48 +1200

    First commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,32 @@
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            {" "}
+            Kevin Pacis
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+64-22-077-2346</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>kevin.pacis001@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

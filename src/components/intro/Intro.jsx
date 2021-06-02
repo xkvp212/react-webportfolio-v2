8128f2b9ba6ac4779355c20525481fdc4a29d938
Kevin <GIT SERVICE EMAIL>
2021-06-03 01:12:13 +1200

    modified topbar and added intro background

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,16 @@
 import "./intro.scss";
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div
+      className="intro"
+      id="intro"
+      style={{
+        backgroundImage: "url(assets/nz-fern-leaves.jpg)",
+        backgroundRepeat: "no-repeat",
+        width: "100%",
+        backgroundSize: "cover"
+      }}
+    ></div>
+  );
 }

63edaa061500dc855142edd289d33ed85a2edac6
Kevin <GIT SERVICE EMAIL>
2021-06-17 13:07:35 +1200

    contact page update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,19 @@
 import "./contact.scss";
 
 export default function Contact() {
-  return <div className="contact" id="contact"></div>;
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="wrapper">
+        <h1>Contact</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit"> Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
